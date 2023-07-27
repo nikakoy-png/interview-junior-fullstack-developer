9197f4c8a4f409f03fa0fc7ae5e765d4bdd325af
nikakoy-png <64211546+nikakoy-png@users.noreply.github.com>
2023-07-28 00:47:46 +0300

    Adding a new cities component with styles, as well as changing the main one. Which interacts with the API.

--- interview-frontend/src/app/app.module.ts
+++ interview-frontend/src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CitiesComponent } from './cities/cities.component';
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import { ErrorModalComponent } from './error-modal/error-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitiesComponent,
+    ErrorModalComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
